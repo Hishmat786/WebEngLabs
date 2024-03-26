cfb667b6a708b61cf37fda20b00092d36a00b61c
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-03-26 15:49:34 +0500

    familier with promise:

--- Week8/practice/Promise.js
+++ Week8/practice/Promise.js
@@ -2,11 +2,11 @@ function getData() {
     return new Promise((resolve, reject) => {
     // Simulating an asynchronous operation (e.g., fetching data from an API)
     setTimeout(() => {
-    //const data = [1, 2, 3, 4, 5];
+    const data = [1, 2, 3, 4, 5];
     // Simulating successful completion
-    let number  = Math.random()
-    return (number>0.5 ? resolve(number): reject("regect"))
-    //resolve(data);
+    // let number  = Math.random()
+    // return (number>0.5 ? resolve(number): reject("regect"))
+    resolve(data);
     // Simulating failure
     //reject(new Error('Failed to fetch data'));
     // reject("reject")
@@ -18,11 +18,12 @@ function getData() {
     // Consuming the Promise
    getData().then(data => {
         const result = data
-        console.log('Data received:', result);
+        console.log("I am in promise");
         //return result
     })
     .catch(error => {
     console.error('Error:', error.message);
     });
+    console.log("I am after promise");
 
-   // console.log(n)
+    //console.log(getData())
