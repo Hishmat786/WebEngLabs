25570b7126500117a6b46e818a71b440cb639952
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-04-29 00:16:30 +0500

    added counter and catagry friend

---
+++ Week11/Task/Week11Tasks/src/Components/GoodBad.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./GoodBad.css";
+export default function GoodBad() {
+  const [good, setGood] = useState([]);
+  const [bad, setBad] = useState([]);
+
+  const handleSubmit = () => {
+    const name = document.getElementById("name").value;
+    const category = document.getElementById("category").value;
+
+    if (category === "good") {
+      setGood([...good, name]);
+    } else if (category === "bad") {
+      setBad([...bad, name]);
+    }
+  };
+  return (
+    <div className="body">
+      <div className="friend">
+        <input type="text" id="name" placeholder="Enter Name" />
+        <select id="category">
+          <option value="good">Good</option>
+          <option value="bad">Bad</option>
+        </select>
+        <br />
+      </div>
+      <button style={{ marginTop: "15px" }} onClick={handleSubmit}>
+        Enter
+      </button>
+      <div className="container">
+        <div className="good">
+          <h3>Good</h3>
+          {good.map((name, index) => (
+            <div key={index}>{name}</div>
+          ))}
+        </div>
+        <div className="bad">
+          <h3>Bad</h3>
+          {bad.map((name, index) => (
+            <div key={index}>{name}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
