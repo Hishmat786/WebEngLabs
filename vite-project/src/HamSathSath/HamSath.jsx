17b44f3c34ebe4e3e441b426362452c8ec1d66b1
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-05-07 17:03:45 +0500

    added work with ham sath game:

--- vite-project/src/HamSathSath/HamSath.jsx
+++ vite-project/src/HamSathSath/HamSath.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 export default function HamSath() {
-    const [user, setuser]= useState([]);
+    const [users, setuser]= useState([]);
     const addUser = () => {
         console.log("clicked");
     };
@@ -9,7 +9,18 @@ export default function HamSath() {
     <div>
       <h1> HamSathSath </h1>
       <button onClick={addUser}>Add User</button>
-      
+      <div>
+        users.map((item) => (
+            <div>
+            <span>User 1</span>
+        <button>Step1</button>
+        <button>Step2</button>
+        <button>Step3</button>
+        <button>Step4</button>
+            </div>
+        ))
+        
+      </div>
     </div>
   );
 }
